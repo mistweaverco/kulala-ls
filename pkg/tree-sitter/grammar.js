09697fcc0d2ef86668528500a69de8f98a4e1506
Marco Kellershoff <marco@mistweaverco.com>
2025-11-24 10:27:42 +0100

    feat(treesitter): allow websocket and grpc methods

--- pkg/tree-sitter/grammar.js
+++ pkg/tree-sitter/grammar.js
@@ -90,7 +90,7 @@ module.exports = grammar({
 
     // LIST http verb is arbitrary and required to use vaultproject
     method: (_) =>
-      /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST|GRAPHQL|WEBSOCKET)/i,
+      /(OPTIONS|GET|HEAD|POST|PUT|DELETE|TRACE|CONNECT|PATCH|LIST|GRAPHQL|WEBSOCKET|WS|WSS|GRPC)/i,
 
     http_version: (_) => prec.dynamic(1, token(prec(0, /HTTP\/[\d\.]+/))),
 
